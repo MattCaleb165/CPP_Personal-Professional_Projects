50414e3f7632b30270b4c672e2ac1ab03acc2911
MattCaleb165 <chm165@live.com>
2024-06-10 20:51:33 -0400

    added more notes; histogram example

--- UDemy_Modern_C++_Course/project-5-section-9/notes.cpp
+++ UDemy_Modern_C++_Course/project-5-section-9/notes.cpp
@@ -106,9 +106,42 @@
 //   {100, 200, 300, 400, 500}
 // };
 
-// for (auto vec: vector_2d) {
-//   for (auto cal: vec) {
+// for (auto vec : vector_2d) {
+//   for (auto val : vec) {
 //     cout << val << " ";
 //   }
 //   cout << endl;
 // }
+
+//Histogram Example
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+    int num_items{};
+    
+    cout << "How many data items do you have? ";
+    cin >> num_items;
+    
+    vector <int> data {};
+    
+    for (int i{1}; i <= num_items; ++i) {
+        int data_item{};
+        cout << "Enter data item " << i << ": ";
+        cin >> data_item;
+        data.push_back(data_item);
+    }
+    cout << "\nDisplaying Histogram" << endl;
+    for (auto val : data) {
+        for (int i{1}; i<=val; ++i) {
+            cout << "-";
+        }
+        cout << endl;
+    }
+    
+    cout << endl;
+    return 0;
+}
