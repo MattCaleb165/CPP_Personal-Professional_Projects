edf638923da9aa1d78e1df734e27aed9f84c00be
MattCaleb165 <caleb.mattingly@alltrails.com>
2024-06-18 18:15:13 -0400

    Adding challenge solution

--- UDemy_Modern_C++_Course/project-6-section10/notes.cpp
+++ UDemy_Modern_C++_Course/project-6-section10/notes.cpp
@@ -18,9 +18,9 @@
 // my_name[5] = 'y' ; // Problem
 
 -----------------------------------------
-char str[80];
+// char str[80];
 
-strcpy(str, "Hello "); //copy
-strcat(str, "there"); //concatenate
-cout << strlen(str); //11
-strcmp(str, "Another"); // > 0
+// strcpy(str, "Hello "); //copy
+// strcat(str, "there"); //concatenate
+// cout << strlen(str); //11
+// strcmp(str, "Another"); // > 0
